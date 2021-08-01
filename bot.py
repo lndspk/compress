34b550bb164b4001ec5f6c7f445791454e91a32f
lndspk <81397853+lndspk@users.noreply.github.com>
2021-08-01 01:55:47 -0500

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,41 @@
+import discord
+from discord.ext import commands
+import asyncio
+import random
+import uuid
+import requests
+import shutil
+import urllib.request
+
+from pygifsicle import optimize
+from pygifsicle import gifsicle
+
+client = commands.Bot(command_prefix = '~')
+
+@client.event
+async def on_ready():
+	print('on')
+
+@client.command()
+async def ping(ctx):
+	await ctx.send(f'{round(client.latency * 1000)}ms')
+
+@client.command()
+async def compress(ctx):
+	imgurl = ctx.message.content.split()[-1]
+	r = requests.get(imgurl, stream=True)
+	imageName = str(uuid.uuid4()) + '.gif'
+	urllib.request.urlretrieve(imgurl, imageName)
+	gifsicle(
+		sources=[imageName],
+		destination=imageName,
+		optimize=True,
+		colors=32,
+		options=["--verbose", "--conserve-memory", "--lossy=80"]
+	)
+	channel = client.get_channel("channel id")
+	await ctx.channel.send(file=discord.File(imageName))
+
+#	await message.channel.send(file=discord.File(imageName))
+
+client.run('ODcwOTE3ODA0OTczNTEwNjg2.YQTvaQ.fVBL4osNAcK2VobFjpHWC7FP89Y')
